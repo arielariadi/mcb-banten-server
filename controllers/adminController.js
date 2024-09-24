ce0b02c1c526bbeb3507633d257fd4b3b136e642
Muhammad Ariel Ariadi <ariel.ariadi123@gmail.com>
2024-09-24 13:40:48 +0700

    feat: add create new task API and task image upload middleware
    - Impemented API for creating new tasks with required field validation. \n- Created multer middleware for handling task image uploads. \n- Configured storage to save images with unique filenames and validated file types.

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,6 +1,11 @@
 import User from '../models/userModel.js';
+import Task from '../models/taskModel.js';
 import asyncHandler from 'express-async-handler';
+import mongoose from 'mongoose';
 
+// @desc Get all users
+// @route GET /v1/admin/list-users
+// @access Private/Admin
 const getAllUsers = asyncHandler(async (req, res) => {
   const users = await User.find().select('-password').lean();
 
@@ -9,4 +14,48 @@ const getAllUsers = asyncHandler(async (req, res) => {
     .json({ status: 'success', message: 'Success get all users', data: users });
 });
 
-export { getAllUsers };
+// @desc Create new Task
+// @route POST /v1/admin/create-task
+// @access Private/Admin
+const createNewTask = asyncHandler(async (req, res) => {
+  const { title, description, socialMediaUrl, reward } = req.body;
+  const image = req.file.path;
+
+  // Confirm data
+  if (!title || !description || !image || !socialMediaUrl || !reward) {
+    return res
+      .status(400)
+      .json({ status: 'fail', message: 'Tolong isi semua data!' });
+  }
+
+  // Check for duplicate title
+  const duplicateTtile = await Task.findOne({ title }).lean().exec();
+  if (duplicateTtile) {
+    return res
+      .status(409)
+      .json({ status: 'fail', message: 'Title sudah digunakan!' });
+  }
+
+  // Create and store new task
+  const task = await Task.create({
+    title,
+    description,
+    image,
+    socialMediaUrl,
+    reward,
+    createdBy: req.user.id,
+  });
+
+  if (task) {
+    // Created
+    return res
+      .status(201)
+      .json({ status: 'success', message: 'Task created!', data: task });
+  } else {
+    return res
+      .status(400)
+      .json({ status: 'fail', message: 'Task gagal dibuat!' });
+  }
+});
+
+export { getAllUsers, createNewTask };
--- routes/admin.js
+++ routes/admin.js
@@ -3,11 +3,21 @@ const router = express.Router();
 
 import { authenticateToken } from '../middleware/authMiddleware.js';
 import isAdmin from '../middleware/adminMiddleware.js';
-import { getAllUsers } from '../controllers/adminController.js';
+import taskImage from '../middleware/taskImageMiddleware.js';
+import { getAllUsers, createNewTask } from '../controllers/adminController.js';
 
 // ENDPOINT INI HANYA BISA DIAKSES OLEH ADMIN SAJA
 
 // Endpoint untuk mendapatkan semua user
 router.get('/list-users', authenticateToken, isAdmin, getAllUsers);
+
+// Endpoint untuk membuat task
+router.post(
+  '/create-task',
+  authenticateToken,
+  isAdmin,
+  taskImage.single('image'),
+  createNewTask,
+);
 
 export default router;
