38bbc60045e885fe193cefad052153516193347c
Muhammad Ariel Ariadi <ariel.ariadi123@gmail.com>
2024-09-26 14:03:54 +0700

    feat: create get task by id API

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,7 @@
 import User from '../models/userModel.js';
 import Task from '../models/taskModel.js';
 import asyncHandler from 'express-async-handler';
+import mongoose from 'mongoose';
 
 // @desc Get all tasks
 // @route GET /v1/user/list-tasks
@@ -21,4 +22,29 @@ const getAllTasks = asyncHandler(async (req, res) => {
   });
 });
 
-export { getAllTasks };
+// @desc Get task by id
+// @route GET /v1/user/task/:id
+// @access Private
+const getTaskById = asyncHandler(async (req, res) => {
+  const { id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return res.status(400).json({ status: 'fail', message: 'ID tidak valid!' });
+  }
+
+  const task = await Task.findById(req.params.id).lean();
+
+  if (!task) {
+    return res
+      .status(400)
+      .json({ status: 'fail', message: 'Tidak ada tugas!' });
+  }
+
+  res.status(200).json({
+    status: 'success',
+    message: 'Sukses mengambil tugas',
+    data: task,
+  });
+});
+
+export { getAllTasks, getTaskById };
--- routes/user.js
+++ routes/user.js
@@ -2,9 +2,12 @@ import express from 'express';
 const router = express.Router();
 
 import { authenticateToken } from '../middleware/authMiddleware.js';
-import { getAllTasks } from '../controllers/userController.js';
+import { getAllTasks, getTaskById } from '../controllers/userController.js';
 
-// // Endpoint untuk mendapatkan semua tugas
+// Endpoint untuk mendapatkan semua tugas
 router.get('/list-tasks', authenticateToken, getAllTasks);
+
+// Endpoint untuk mendapatkan tugas berdasarkan ID
+router.get('/task/:id', authenticateToken, getTaskById);
 
 export default router;
